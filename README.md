9dcba6a4cfb5986390958317bd6643208af18f91
circa10a <caleblemoine@gmail.comS>
2018-01-17 18:25:07 -0600

    cleaned up

---
+++ README.md
@@ -0,0 +1,13 @@
+# CoinPicker
+
+Determine most profitable cryptocurrency
+
+## Usage
+
+```bash
+python MinerControl.py --coin Ethereum --file ./approvedCoinFile
+``` 
+
+## Requirements
+
+Add coins you're interested in to a file, separated by \n
---
+++ coinPicker.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python
+import requests
+import argparse,os.path,platform
+from termcolor import colored
+from subprocess import call
+
+def determineOS():
+    osType = platform.platform()
+    if 'Windows' in osType:
+        print('windows')
+    elif 'Darwin' in osType:
+        print('mac')
+    else:
+        print('linux')
+
+def parser():
+    parser = argparse.ArgumentParser(description='coinPicker')
+    parser.add_argument('-c','--coin', help='Which Coin to Mine', required=True)
+    parser.add_argument('-f','--file', help='List of approved coins', required=True)
+    return parser.parse_args()
+
+def checkFileExists(file):
+     if not os.path.exists(file):
+         print(colored('Path to file is invalid.', 'red'))
+         exit(1)
+
+def fetchCoinJson(coinToMine,approvedCoinFile):
+    try:
+        r = requests.get('http://whattomine.com/coins.json')
+    except:
+        print(colored('Unable to connect to whattomine.com', 'red'))
+        exit(1)
+    rJson = r.json()['coins']
+    coinList = open(approvedCoinFile, 'r')
+    for coin in coinList:
+        coin = coin.strip()
+        print(coin + ' = ' + str(rJson[coin]['profitability']) + ' :: ' + coinToMine + ' = ' + str(rJson[coinToMine]['profitability']))
+        if rJson[coin]['profitability'] > rJson[coinToMine]['profitability']:
+            coinToMine = coin
+            exe = './' + coinToMine + '.sh'
+            print(exe)
+
+if __name__ == '__main__':
+    try:
+        args = parser()
+        #convert first arg character to uppercase
+        coin = args.coin.title()
+        coinFile = args.file
+        checkFileExists(coinFile)
+        fetchCoinJson(coin,coinFile)
+    except:
+        print (colored('An error has occurred', 'red'))
